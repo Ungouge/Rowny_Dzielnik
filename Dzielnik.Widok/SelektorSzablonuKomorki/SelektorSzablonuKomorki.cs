6d61ed3e92d4b5254542ba1b189de6a9876318f3
Ungouge <zlobik89@gmail.com>
2019-09-30 04:26:21 +0200

    Dodano klasę SelektorSzablonuKomorki

---
+++ Dzielnik.Widok/SelektorSzablonuKomorki/SelektorSzablonuKomorki.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using Dzielnik.ModelWidoku;
+
+namespace Dzielnik.Widok.SelektorSzablonuKomorki
+{
+    class SelektorSzablonuKomorki: DataTemplateSelector
+    {
+        public DataTemplate SzablonKomorkiWejsciowejImienia { get; set; }
+        public DataTemplate SzablonKomorkiKwotyWejsciowej { get; set; }
+        public DataTemplate SzablonKomorkiImienia { get; set; }
+        public DataTemplate SzablonKomorkiWyjsciowej { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item != null)
+                if (item is IKomorka Komorka)
+                    switch (Komorka)
+                    {
+                        case IKomorkaWejsciowaImienia komorkaWejsciowaImienia:
+                            return SzablonKomorkiWejsciowejImienia;
+                        case IKomorkaKwotyWejsciowej komorkaKwotyWejsciowej:
+                            return SzablonKomorkiKwotyWejsciowej;
+                        case IKomorkaImienia komorkaImienia:
+                            return SzablonKomorkiImienia;
+                        case IKomorkaWyjsciowa komorkaWyjsciowa:
+                            return SzablonKomorkiWyjsciowej; 
+                    }
+
+            return null;
+        }
+    }
+}
