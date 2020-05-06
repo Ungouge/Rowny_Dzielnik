1e0ac490b6d96b70cbb98e696671d50e26d242bd
Ungouge <zlobik89@gmail.com>
2020-05-07 00:47:54 +0200

    Dodadno fabryka dla OsobaPieniezna.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/osoby/osobaPieniezna/IFabrykaOsobaPieniezna.cs
@@ -0,0 +1,16 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.Osoby;
+
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla fabryk osob pienieznych.
+    /// </summary>
+    public interface IFabrykaOsobaPieniezna : IFabrykaOsoba
+    {
+        /// <summary>
+        /// Stworz nowa instancje osoby pienieznej.
+        /// </summary>
+        IOsobaPienieznaWymienna StworzOsoba(byte iD, INaleznoscPieniezna naleznosc);
+    }
+}
