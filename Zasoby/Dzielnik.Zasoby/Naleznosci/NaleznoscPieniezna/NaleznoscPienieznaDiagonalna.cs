b264f4f50d39c194b68dcacc90131bde66fb56a9
Ungouge <zlobik89@gmail.com>
2020-05-06 14:51:48 +0200

    Dodano NaleznoscPienieznaDiagonalna.

---
+++ Zasoby/Dzielnik.Zasoby/Naleznosci/NaleznoscPieniezna/NaleznoscPienieznaDiagonalna.cs
@@ -0,0 +1,19 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+namespace Dzielnik.Zasoby.Naleznosci
+{
+    /// <summary>
+    /// Kukla odpowiadjaca naleznosci, ze swiadczeniem pieniznym, osoby wzgledem samej siebie,
+    /// ktora to powinna znajdowac sie na diagonalnej tabel z naleznosciami osob.
+    /// </summary>
+    internal class NaleznoscPienieznaDiagonalna : INaleznoscPienieznaDiagonalnaSwiadczenie
+    {
+        public int Swiadczenie
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
