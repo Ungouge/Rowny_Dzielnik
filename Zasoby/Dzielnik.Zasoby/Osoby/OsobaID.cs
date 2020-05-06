21e0ffb3ad3af52df2518fb03f1eb9d721e90294
Ungouge <zlobik89@gmail.com>
2020-05-06 21:48:27 +0200

    Dodano abstrakcje OsobaID.

---
+++ Zasoby/Dzielnik.Zasoby/Osoby/OsobaID.cs
@@ -0,0 +1,23 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Osoby;
+
+namespace Dzielnik.Zasoby.Osoby
+{
+    /// <summary>
+    /// Abstrakcyjna klasa dla osob z numer identyfikacyjny,
+    /// ktory powinien odpowiadac numerowi kolejnosci w tabeli z danymi wejsciowymi. 
+    /// </summary>
+    internal abstract class OsobaID : IOsobaID
+    {
+        public byte ID { get; }
+
+        protected OsobaID(byte iD)
+        {
+            ID = iD;
+        }
+
+        public bool CzyToTaSamaOsoba(IOsobaID osobaID)
+        {
+            return ID == osobaID.ID;
+        }
+    }
+}
