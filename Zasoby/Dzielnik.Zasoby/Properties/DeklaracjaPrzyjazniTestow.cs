6619d5fb1c454349341d0c8703a51d85e334e6be
Ungouge <zlobik89@gmail.com>
2020-05-07 01:10:27 +0200

    Dodano deklaracje pzyjaznie testow dla Dzielnik.Zasoby.

---
+++ Zasoby/Dzielnik.Zasoby/Properties/DeklaracjaPrzyjazniTestow.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+// Argumety projektu dające dostęp do wewnetrznych zasobów projektom testów jednostkowych oraz zestawu do tworzenia atrap.
+
+[assembly: InternalsVisibleTo("Dzielnik.Testy.Zasoby.stworz")]
+[assembly: InternalsVisibleTo("Dzielnik.Testy.Zasoby.stworzMocki")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
