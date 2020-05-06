3f5e29cf1c346d7124dd719a0f2a64124a152a7d
Ungouge <zlobik89@gmail.com>
2020-05-06 14:53:57 +0200

    Dodano interfejsy dla fabryk bazowych naleznosci.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/naleznosci/IFabrykaNaleznoscDiagonalna.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs bazowy dla fabryk naleznosci diagonalnych. 
+    /// </summary>
+    public interface IFabrykaNaleznoscDiagonalna
+    {
+    }
+}
---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/naleznosci/IFabrykaNaleznosciZerowa.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs bazowy dla fabryk naleznosci zerowych.
+    /// </summary>
+    public interface IFabrykaNaleznosciZerowa
+    {
+    }
+}
