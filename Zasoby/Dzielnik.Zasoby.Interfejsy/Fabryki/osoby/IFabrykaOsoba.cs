0661033804593a3bc1585437d4aff23a16332f8a
Ungouge <zlobik89@gmail.com>
2020-05-07 00:46:23 +0200

    Dodano interfejs bazowy dla fabryk osob.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/osoby/IFabrykaOsoba.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs bazowy dla fabryk osob.
+    /// </summary>
+    public interface IFabrykaOsoba
+    {
+    }
+}
