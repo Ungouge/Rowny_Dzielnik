63aea9e4286459b008443f77a8103ebfc06d6940
Ungouge <zlobik89@gmail.com>
2020-05-06 21:49:10 +0200

    Dodano OsobaPieniezna.

---
+++ Zasoby/Dzielnik.Zasoby/Osoby/OsobaPieniezna/OsobaPieniezna.cs
@@ -0,0 +1,28 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.Osoby;
+
+namespace Dzielnik.Zasoby.Osoby
+{
+    /// <summary>
+    /// Osoba posiadajca wplate swiadczenia pienieznym.
+    /// </summary>
+    internal class OsobaPieniezna: OsobaID, IOsobaPienieznaWymienna, IOsobaPieniezna
+    {
+        public INaleznoscPieniezna Wplata { get; set; }
+
+        public OsobaPieniezna(byte iD, INaleznoscPieniezna naleznosc): base(iD)
+        {
+            Wplata = naleznosc;
+        }
+
+        public IOsobaPienieznaWymienna DodajNaleznoscPienieznaOsobie(INaleznoscPieniezna naleznosc)
+        {
+            return new OsobaPieniezna(ID, Wplata.Suma(naleznosc));
+        }
+
+        public IOsobaPienieznaWymienna OdejmnijNaleznoscPienieznaOsobie(INaleznoscPieniezna naleznosc)
+        {
+            return new OsobaPieniezna(ID, Wplata.Roznica(naleznosc));
+        }
+    }
+}
