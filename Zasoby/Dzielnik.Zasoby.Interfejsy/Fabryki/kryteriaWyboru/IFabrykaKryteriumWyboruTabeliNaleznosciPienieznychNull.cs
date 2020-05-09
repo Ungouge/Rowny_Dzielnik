1175bf43a17c227659c85a2ccd47fa90ef9a241e
Ungouge <zlobik89@gmail.com>
2020-05-10 00:15:45 +0200

    Dodano IFabrykaKryteriumWyboruTabeliNaleznosciPienieznychNull.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/kryteriaWyboru/IFabrykaKryteriumWyboruTabeliNaleznosciPienieznychNull.cs
@@ -0,0 +1,15 @@
+﻿using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interface dla fabryk kryteriow wyboru z naleznoscia pieniezna 
+    /// </summary>
+    public interface IFabrykaKryteriumWyboruTabeliNaleznosciPienieznychNull
+    {
+        /// <summary>
+        /// Tworzy instancje kryteriium wyboru tabel naleznosci ze swiadczeniem pienieznym z zachowaniem pustym.
+        /// </summary>
+        IKryteriumWyboruTabeliNaleznosciPienieznej StworzKryteriumWyboruTabeliNaleznosciPienieznychNull();
+    }
+}
