d2f4d2ff5b20ac89455c42a467e3da1e4ac5804d
Ungouge <zlobik89@gmail.com>
2020-05-10 00:17:51 +0200

    Dodano FabrykaKryteriumWyboruTabeliNaleznosciPienieznychNull.

---
+++ Zasoby/Dzielnik.Zasoby/Fabryki/kryteriaWyboru/FabrykaKryteriumWyboruTabeliNaleznosciPienieznychNull.cs
@@ -0,0 +1,17 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+using Dzielnik.Zasoby.KryteriumWyboru;
+
+namespace Dzielnik.Zasoby.Fabryki
+{
+    /// <summary>
+    /// Interface dla fabryk kryteriow wyboru ze swiadczeniem pienieznym z zachowaniem pustym.
+    /// </summary>
+    public class FabrykaKryteriumWyboruTabeliNaleznosciPienieznychNull : IFabrykaKryteriumWyboruTabeliNaleznosciPienieznychNull
+    {
+        public IKryteriumWyboruTabeliNaleznosciPienieznej StworzKryteriumWyboruTabeliNaleznosciPienieznychNull()
+        {
+            return new KryteriumWyboruTabeliNaleznosciPienieznychNull();
+        }
+    }
+}
