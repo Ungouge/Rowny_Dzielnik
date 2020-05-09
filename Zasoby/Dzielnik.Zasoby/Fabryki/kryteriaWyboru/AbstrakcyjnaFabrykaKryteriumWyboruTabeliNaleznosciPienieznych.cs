04aa619fe19711972976902d22f3192d6df4f05d
Ungouge <zlobik89@gmail.com>
2020-05-10 00:17:29 +0200

    Dodano AbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych.

---
+++ Zasoby/Dzielnik.Zasoby/Fabryki/kryteriaWyboru/AbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych.cs
@@ -0,0 +1,32 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+using Dzielnik.Zasoby.KryteriumWyboru;
+
+namespace Dzielnik.Zasoby.Fabryki
+{
+    /// <summary>
+    /// Abstrakcyjna fabryka kryteriow wyboru tabeli naleznosci.
+    /// </summary>
+    public class AbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych: IAbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych
+    {
+        private readonly IFabrykaNaleznoscPienieznaZerowa fabrykaNaleznoscPieniezna;
+
+        public AbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych(IFabrykaNaleznoscPienieznaZerowa fabrykaNaleznoscPieniezna)
+        {
+            this.fabrykaNaleznoscPieniezna = fabrykaNaleznoscPieniezna;
+        }
+
+        public IKryteriumWyboruTabeliNaleznosciPienieznej StworzKryteriumWyboruTabeliNaleznosci(KryteriumWyboruTabeliNaleznosciWylicznik kryteriumWyboruTabeliNaleznosci )
+        {
+            switch (kryteriumWyboruTabeliNaleznosci)
+            {
+                case KryteriumWyboruTabeliNaleznosciWylicznik.KryteriumSumyWymienionejNaleznosci:
+                    goto default;
+                case KryteriumWyboruTabeliNaleznosciWylicznik.IlosciWymian:
+                    return new KryteriumIlosciWymian();
+                default:
+                    return new KryteriumSumyWymienionejNaleznosciPienieznej(fabrykaNaleznoscPieniezna.StworzNaleznoscZerowa());
+            }
+        }
+    }
+}
