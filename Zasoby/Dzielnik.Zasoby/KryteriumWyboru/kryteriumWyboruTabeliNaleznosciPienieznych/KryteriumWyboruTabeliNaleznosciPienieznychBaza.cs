dd1da783b97bbc02ff979d1d878f3bc1b6125f4d
Ungouge <zlobik89@gmail.com>
2020-05-10 00:09:05 +0200

    Dodano KryteriumWyboruTabeliNaleznosciPienieznychBaza.

---
+++ Zasoby/Dzielnik.Zasoby/KryteriumWyboru/kryteriumWyboruTabeliNaleznosciPienieznych/KryteriumWyboruTabeliNaleznosciPienieznychBaza.cs
@@ -0,0 +1,21 @@
+﻿using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+using System;
+
+namespace Dzielnik.Zasoby.KryteriumWyboru
+{
+    /// <summary>
+    /// Abstrakcja kryteriow wyboru tabel naleznosci ze swiadczeniem pienieznym.
+    /// </summary>
+    internal abstract class KryteriumWyboruTabeliNaleznosciPienieznychBaza : IKryteriumWyboruTabeliNaleznosciPienieznej
+    {
+        public abstract bool CzyPorownywaneKyrteriumLepsze(IKryteriumWyboruTabeliNaleznosciPienieznej porownywaneKryterium);
+        
+        bool IKryteriumWyboruTabeliNaleznosci.CzyPorownywaneKyrteriumLepsze(IKryteriumWyboruTabeliNaleznosci porownywaneKryterium)
+        {
+            if (porownywaneKryterium is IKryteriumWyboruTabeliNaleznosciPienieznej kryteriumWyboruTabeliNaleznosciPienieznej)
+                return (this as IKryteriumWyboruTabeliNaleznosciPienieznej).CzyPorownywaneKyrteriumLepsze(kryteriumWyboruTabeliNaleznosciPienieznej);
+
+            throw new TypeAccessException();
+        }
+    }
+}
