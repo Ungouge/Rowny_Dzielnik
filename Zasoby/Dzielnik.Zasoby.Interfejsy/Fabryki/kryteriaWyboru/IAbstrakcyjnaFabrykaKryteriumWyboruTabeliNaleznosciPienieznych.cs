60fc11d30cd62eb6cf71455ec2f1dd397bd356b2
Ungouge <zlobik89@gmail.com>
2020-05-10 00:15:20 +0200

    Dodano IAbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/kryteriaWyboru/IAbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych.cs
@@ -0,0 +1,15 @@
+﻿using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla abstrakcyjnych fabryk kryteriow wyboru tabeli naleznosci.
+    /// </summary>
+    public interface IAbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych
+    {
+        /// <summary>
+        /// Stworz instancje kryterium wyboru tabeli naleznosci odpowiedni przeslanemu wylicznikowi kryterium.
+        /// </summary>
+        IKryteriumWyboruTabeliNaleznosciPienieznej StworzKryteriumWyboruTabeliNaleznosci(KryteriumWyboruTabeliNaleznosciWylicznik kryteriumWyboruTabeliNaleznosci);
+    }
+}
