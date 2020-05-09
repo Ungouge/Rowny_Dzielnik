2742554e19fd9f90b81f55d60dcb74e340b8cde6
Ungouge <zlobik89@gmail.com>
2020-05-10 00:11:58 +0200

    Dodano KryteriumWyboruTabeliNaleznosciPienieznychNull.

---
+++ Zasoby/Dzielnik.Zasoby/KryteriumWyboru/kryteriumWyboruTabeliNaleznosciPienieznych/KryteriumWyboruTabeliNaleznosciPienieznychNull.cs
@@ -0,0 +1,15 @@
+﻿using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+
+namespace Dzielnik.Zasoby.KryteriumWyboru
+{
+    /// <summary>
+    /// Kryteriow wyboru tabel naleznosci ze swiadczeniem pienieznym z zachowaniem pustym.
+    /// </summary>
+    internal class KryteriumWyboruTabeliNaleznosciPienieznychNull: KryteriumWyboruTabeliNaleznosciPienieznychBaza, IKryteriumWyboruTabeliNaleznosciPienieznej
+    {
+        public override bool CzyPorownywaneKyrteriumLepsze(IKryteriumWyboruTabeliNaleznosciPienieznej porownywaneKryterium)
+        {
+            return false;
+        }
+    }
+}
