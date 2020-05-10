e2d16f625ae465f977f2cc00fdf736f273df7ede
Ungouge <zlobik89@gmail.com>
2020-05-10 22:30:11 +0200

    Dodano ITabelaNaleznosciPienieznejNull.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/TabeleNaleznosci/tabelaNaleznosciPienieznej/ITabelaNaleznosciPienieznejNull.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Dzielnik.Model.Interfejsy.TabeleNaleznosci
+{
+    /// <summary>
+    /// Interfejs dla dwuwymiarowych tabel zawierajacych naleznosci pienieznej osobom względem siebie z zachowaniem pustym.
+    /// </summary>
+    internal interface ITabelaNaleznosciPienieznejNull : ITabelaNaleznosciPienieznej
+    {
+    }
+}
