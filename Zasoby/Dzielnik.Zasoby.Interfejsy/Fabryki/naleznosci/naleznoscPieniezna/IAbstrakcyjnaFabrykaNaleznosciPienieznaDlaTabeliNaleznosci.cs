e506d589548c1ba83bbe3970757ade1eb6ee2210
Ungouge <zlobik89@gmail.com>
2020-05-10 22:32:45 +0200

    Dodano INaleznoscPienieznaSwiadczenie.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/naleznosci/naleznoscPieniezna/IAbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci.cs
@@ -0,0 +1,21 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla abstrakcyjnej fabryki dla tabel naleznosci pieniezncyh do tworzenia naleznosci w zaleznosci od polozenia w tabeli.
+    /// </summary>
+    public interface IAbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci: IAbstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci
+    {
+        /// <summary>
+        /// Stworz nowa instancje naleznosci zerowej lub diagonalnej w zaleznosci od docelowego polozenia w tabeli.
+        /// </summary>
+        INaleznoscPienieznaSwiadczenie StworzNaleznoscPienieznaZwrotna(IPolozenieGornotrojkatneWTabeliNaleznosci polozenieWTabeli);
+
+        /// <summary>
+        /// Stworz nowa instancje naleznosci zerowej.
+        /// </summary>
+        INaleznoscPieniezna StworzNaleznoscPienieznaZerowa();
+    }
+}
