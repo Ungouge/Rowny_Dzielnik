6a5a06361ba1c1462028d59c20c5de216193e02f
Ungouge <zlobik89@gmail.com>
2020-05-10 13:43:55 +0200

    Dodano IPolozenieWTabeliNaleznosci.cs.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/TabeleNaleznosci/polozenieWTabeliNaleznosci/IPolozenieWTabeliNaleznosci.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci
+{
+    /// <summary>
+    /// Interfejs do struktury zawierajacej koordynaty w tabeli naleznosci.
+    /// </summary>
+    public interface IPolozenieWTabeliNaleznosci: IPolozenieGornotrojkatneWTabeliNaleznosci
+    {
+        new byte Indeks_X { get; }
+
+        new byte Indeks_Y { get; }
+    }
+}
