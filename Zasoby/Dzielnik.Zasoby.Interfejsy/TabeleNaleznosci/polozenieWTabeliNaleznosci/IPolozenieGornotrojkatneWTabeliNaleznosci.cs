a1c979bff50ab65a6da512e326f70a3f0bd3f345
Ungouge <zlobik89@gmail.com>
2020-05-10 13:43:24 +0200

    Dodano IPolozenieGornotrojkatneWTabeliNaleznosci.cs.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/TabeleNaleznosci/polozenieWTabeliNaleznosci/IPolozenieGornotrojkatneWTabeliNaleznosci.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci
+{
+    /// <summary>
+    /// Interfejs do struktury zawierajacej koordynaty gornotrojkatne w tabeli naleznosci.
+    /// </summary>
+    public interface IPolozenieGornotrojkatneWTabeliNaleznosci
+    {
+        byte Indeks_X { get; }
+
+        byte Indeks_Y { get; }
+
+        /// <summary>
+        /// Stwierdza czy przechowywabe koordynaty sa polozone na osi diagonalnej tabeli. Czyli czy oba koordynaty sa sobie rowne.
+        /// </summary>
+        bool CzyKoordynatyDiagonalne { get; }
+
+        /// <summary>
+        /// Stwierdza czy przechowywabe koordynaty sa polozone na gornymtrojkacie tabeli. Czyli czy indeks na osi X jest wiekszy od tego osi Y.
+        /// </summary>
+        bool CzyKoordynatyGorntrojkatne { get; }
+    }
+}
