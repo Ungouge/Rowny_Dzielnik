a9343a9c885640a24a2a3ff0b02e2109b20a5d0c
Ungouge <zlobik89@gmail.com>
2020-05-10 22:28:28 +0200

    Dodano ITabelaNaleznosci.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/TabeleNaleznosci/ITabelaNaleznosci.cs
@@ -0,0 +1,11 @@
+﻿using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+
+namespace Dzielnik.Model.Interfejsy.TabeleNaleznosci
+{
+    /// <summary>
+    /// Interfejs dla dwuwymiarowych tabel zawierajacych naleznosci odpowiedniego typu osobom względem siebie.
+    /// </summary>
+    internal interface ITabelaNaleznosci: ITabelaNaleznosciZwrotna
+    {
+    }
+}
