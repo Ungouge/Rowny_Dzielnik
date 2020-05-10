ac7a1d45c6ce04317e5e9793e10a4f84b462a634
Ungouge <zlobik89@gmail.com>
2020-05-10 23:50:26 +0200

    Dodano FabrykaTabelaNalezosciPienieznej.

---
+++ Model/Dzielnik.Model/Fabryki/tabelaNaleznosci/FabrykaTabelaNalezosciPienieznej.cs
@@ -0,0 +1,34 @@
+﻿using Dzielnik.Model.Interfejsy.Fabryki;
+using Dzielnik.Model.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Model.TabelaNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+
+namespace Dzielnik.Model.Fabryki
+{
+    /// <summary>
+    /// Fabryka do tworzenia TabelaNaleznosciPienieznej.
+    /// </summary>
+    internal class FabrykaTabelaNalezosciPienieznej : IFabrykaTabelaNalezosciPienieznej
+    {
+        private readonly IAbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych fabrykaKryteriumWyboruTabeliNaleznosci;
+
+        private readonly IAbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci;
+
+        public FabrykaTabelaNalezosciPienieznej(IAbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych fabrykaKryteriumWyboruTabeliNaleznosci,
+            IAbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci)
+        {
+            this.fabrykaKryteriumWyboruTabeliNaleznosci = fabrykaKryteriumWyboruTabeliNaleznosci;
+
+            this.abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci = abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci;
+        }
+
+        public ITabelaNaleznosciPienieznej StworzTabelaNaleznosci(byte iloscOsob, KryteriumWyboruTabeliNaleznosciWylicznik kryteriumWyboruTabeliNaleznosci)
+        {
+            IKryteriumWyboruTabeliNaleznosciPienieznej kryterium =
+                fabrykaKryteriumWyboruTabeliNaleznosci.StworzKryteriumWyboruTabeliNaleznosci(kryteriumWyboruTabeliNaleznosci);
+
+            return new TabelaNaleznosciPienieznej(iloscOsob, kryterium, abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci);
+        }
+    }
+}
