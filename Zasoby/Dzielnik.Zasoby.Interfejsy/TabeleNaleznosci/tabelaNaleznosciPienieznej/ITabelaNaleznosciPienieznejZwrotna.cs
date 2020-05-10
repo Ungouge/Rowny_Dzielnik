14597a98406b8d699d0f4ad6404ddfb7b3ddf9c4
Ungouge <zlobik89@gmail.com>
2020-05-10 22:27:42 +0200

    Dodano ITabelaNaleznosciPienieznejZwrotna.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/TabeleNaleznosci/tabelaNaleznosciPienieznej/ITabelaNaleznosciPienieznejZwrotna.cs
@@ -0,0 +1,15 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+namespace Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci
+{
+    /// <summary>
+    /// Interfejs dla zwrotu do modelu widoku dwuwymiarowych tabel zawierajacych naleznosci pienieznej osobom względem siebie.
+    /// </summary>
+    public interface ITabelaNaleznosciPienieznejZwrotna : ITabelaNaleznosciBaza, ITabelaNaleznosciZwrotna
+    {
+        /// <summary>
+        /// Zwroc wartossc dwuwymiarowej tabeli zawierajaca naleznosci pienieznej podanego polozenia.
+        /// </summary>
+        INaleznoscPienieznaSwiadczenie this[IPolozenieWTabeliNaleznosci polozenieWTabeli] { get; }
+    }
+}
