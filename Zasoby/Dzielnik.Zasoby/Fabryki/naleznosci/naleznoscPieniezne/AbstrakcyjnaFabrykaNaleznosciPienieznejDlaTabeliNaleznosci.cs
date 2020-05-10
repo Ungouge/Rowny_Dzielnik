6215c92ba26950c4dd6979260fecd65fecf3ce55
Ungouge <zlobik89@gmail.com>
2020-05-10 22:33:40 +0200

    Dodano AbstrakcyjnaFabrykaNaleznosciPienieznejDlaTabeliNaleznosci.

---
+++ Zasoby/Dzielnik.Zasoby/Fabryki/naleznosci/naleznoscPieniezne/AbstrakcyjnaFabrykaNaleznosciPienieznejDlaTabeliNaleznosci.cs
@@ -0,0 +1,37 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+
+namespace Dzielnik.Zasoby.Fabryki
+{
+    /// <summary>
+    /// Abstrakcyjna fabryka dla tabel naleznosci pieniezncyh do tworzenia naleznosci w zaleznosci od polozenia w tabeli.
+    /// </summary>
+    public class AbstrakcyjnaFabrykaNaleznosciPienieznejDlaTabeliNaleznosci : IAbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci
+    {
+        private readonly IFabrykaNaleznoscPienieznaDiagonalna fabrykaNaleznoscPienieznaDiagonalna;
+
+        private readonly IFabrykaNaleznoscPienieznaZerowa fabrykaNaleznoscPienieznaZerowa;
+
+        public AbstrakcyjnaFabrykaNaleznosciPienieznejDlaTabeliNaleznosci
+            (IFabrykaNaleznoscPienieznaDiagonalna fabrykaNaleznoscPienieznaDiagonalna, IFabrykaNaleznoscPienieznaZerowa fabrykaNaleznoscPienieznaZerowa)
+        {
+            this.fabrykaNaleznoscPienieznaDiagonalna = fabrykaNaleznoscPienieznaDiagonalna;
+
+            this.fabrykaNaleznoscPienieznaZerowa = fabrykaNaleznoscPienieznaZerowa;
+        }
+
+        public INaleznoscPienieznaSwiadczenie StworzNaleznoscPienieznaZwrotna(IPolozenieGornotrojkatneWTabeliNaleznosci polozenieWTabeli)
+        {
+            if (polozenieWTabeli.CzyKoordynatyDiagonalne == true)
+                return fabrykaNaleznoscPienieznaDiagonalna.StworzNaleznoscDiagonalna();
+
+            return StworzNaleznoscPienieznaZerowa();
+        }
+
+        public INaleznoscPieniezna StworzNaleznoscPienieznaZerowa()
+        {
+            return fabrykaNaleznoscPienieznaZerowa.StworzNaleznoscZerowa();
+        }
+    }
+}
