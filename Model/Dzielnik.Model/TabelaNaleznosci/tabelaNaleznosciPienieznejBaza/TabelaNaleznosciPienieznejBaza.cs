4f19daba02c8d73f2546539742d0dcb6bc44eca2
Ungouge <zlobik89@gmail.com>
2020-05-10 23:43:51 +0200

    Dodano TabelaNaleznosciPienieznejBaza.

---
+++ Model/Dzielnik.Model/TabelaNaleznosci/tabelaNaleznosciPienieznejBaza/TabelaNaleznosciPienieznejBaza.cs
@@ -0,0 +1,22 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Fabryki;
+
+namespace Dzielnik.Model.TabelaNaleznosci
+{
+    /// <summary>
+    /// abstrakcyjna 
+    /// </summary>
+    internal abstract partial class TabelaNaleznosciPienieznejBaza
+    {
+        public byte Rozmiar { get; }
+
+        protected readonly IAbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci;
+
+        protected TabelaNaleznosciPienieznejBaza(byte iloscOsob, 
+            IAbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci)
+        {
+            Rozmiar = iloscOsob;
+
+            this.abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci = abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci;
+        }
+    }
+}
---
+++ Model/Dzielnik.Model/TabelaNaleznosci/tabelaNaleznosciPienieznejBaza/tenIndekstatorPolozenia.cs
@@ -0,0 +1,18 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+
+namespace Dzielnik.Model.TabelaNaleznosci
+{
+    partial class TabelaNaleznosciPienieznejBaza
+    {
+        public virtual INaleznoscPienieznaSwiadczenie this[IPolozenieWTabeliNaleznosci polozenieWTabeli]
+        {
+            get
+            {
+                return polozenieWTabeli.CzyKoordynatyDiagonalne ?
+                    abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci.StworzNaleznoscPienieznaZwrotna(polozenieWTabeli)
+                    : abstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci.StworzNaleznoscPienieznaZerowa();
+            }
+        }
+    }
+}
