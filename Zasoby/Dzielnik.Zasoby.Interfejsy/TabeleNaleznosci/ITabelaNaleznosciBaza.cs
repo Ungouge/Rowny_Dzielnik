bc04181a1af1025157f40485feb22e2ff9039e0e
Ungouge <zlobik89@gmail.com>
2020-05-10 22:21:11 +0200

    Dodano ITabelaNaleznosciBaza.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/TabeleNaleznosci/ITabelaNaleznosciBaza.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci
+{
+    /// <summary>
+    /// Interfejs bazowy dla dwuwymiarowych tabel zawierajacych naleznosci odpowiedniego typu osobom względem siebie.
+    /// </summary>
+    public interface ITabelaNaleznosciBaza
+    {
+        /// <summary>
+        /// Zwraca ilosc komorek w jednym wymiarze tabeli.
+        /// </summary>
+        byte Rozmiar { get; }
+    }
+}
