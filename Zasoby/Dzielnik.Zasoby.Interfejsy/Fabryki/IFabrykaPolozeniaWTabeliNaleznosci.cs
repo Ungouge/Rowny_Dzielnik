147da3bd99de79c02fa15f9fa72cea154bec12b8
Ungouge <zlobik89@gmail.com>
2020-05-10 13:48:21 +0200

    Dodano IFabrykaPolozeniaWTabeliNaleznosci.cs.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/IFabrykaPolozeniaWTabeliNaleznosci.cs
@@ -0,0 +1,15 @@
+﻿using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs fabryka do tworzenia struktury zawierajacej koordynaty w tabeli naleznosci.
+    /// </summary>
+    public interface IFabrykaPolozeniaWTabeliNaleznosci
+    {
+        /// <summary>
+        /// Stworz instancje struktury zawierajacej koordynaty w tabeli naleznosci.
+        /// </summary>
+        IPolozenieWTabeliNaleznosci StworzPolozeniaWTabeliNaleznosci(byte indeks_X, byte indeks_Y);
+    }
+}
