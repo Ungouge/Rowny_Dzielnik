edf5ff2a6e44b374643239d4754ef62d0aa4eaa4
Ungouge <zlobik89@gmail.com>
2020-05-10 22:31:25 +0200

    Dodano IAbstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Fabryki/naleznosci/IAbstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Dzielnik.Zasoby.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla abstrakcyjnej fabryki dla tabel naleznosci do tworzenia naleznosci w zaleznosci od polozenia w tabeli.
+    /// </summary>
+    public interface IAbstrakcyjnaFabrykaNaleznosciDlaTabeliNaleznosci
+    {
+    }
+}
