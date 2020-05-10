cdcf1fe989261227f745f9d4bd843921dc89926e
Ungouge <zlobik89@gmail.com>
2020-05-10 13:48:58 +0200

    Dodano FabrykaPolozeniaWTabeliNaleznosci.cs.

---
+++ Zasoby/Dzielnik.Zasoby/Fabryki/FabrykaPolozeniaWTabeliNaleznosci.cs
@@ -0,0 +1,17 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.TabeleNaleznosci;
+
+namespace Dzielnik.Zasoby.Fabryki
+{
+    /// <summary>
+    /// Fabryka do tworzenia struktury zawierajacej koordynaty w tabeli naleznosci.
+    /// </summary>
+    public class FabrykaPolozeniaWTabeliNaleznosci : IFabrykaPolozeniaWTabeliNaleznosci
+    {
+        public IPolozenieWTabeliNaleznosci StworzPolozeniaWTabeliNaleznosci(byte indeks_X, byte indeks_Y)
+        {
+            return new PolozenieWTabeliNaleznosci(indeks_X, indeks_Y);
+        }
+    }
+}
