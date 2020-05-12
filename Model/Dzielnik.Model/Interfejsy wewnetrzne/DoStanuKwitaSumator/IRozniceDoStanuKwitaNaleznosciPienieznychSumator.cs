7012e7abb7439a6907241ea0e85e3f638f95df0f
Ungouge <zlobik89@gmail.com>
2020-05-12 16:17:09 +0200

    Dodano IRozniceDoStanuKwitaNaleznosciPienieznychSumator.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/DoStanuKwitaSumator/IRozniceDoStanuKwitaNaleznosciPienieznychSumator.cs
@@ -0,0 +1,15 @@
+﻿using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.Interfejsy.DoStanuKwitaSumator
+{
+    /// <summary>
+    /// Interfejs dla sumatora roznic naleznosci pienieznej tablic osob od stanu kwita.
+    /// </summary>
+    internal interface IRozniceDoStanuKwitaNaleznosciPienieznychSumator
+    {
+        /// <summary>
+        /// Zwraca sume roznic naleznosci pienieznej podanej  tablic osob od stanu kwita.
+        /// </summary>
+        long SumujRoznice(ITablicaOsobPienieznychIterowalna tablicaOsob);
+    }
+}
