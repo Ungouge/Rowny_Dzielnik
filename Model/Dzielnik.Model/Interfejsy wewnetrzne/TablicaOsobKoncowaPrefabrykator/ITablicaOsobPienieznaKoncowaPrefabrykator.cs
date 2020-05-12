794bcd4c8de90aa9ef88253584cade78fe59f97f
Ungouge <zlobik89@gmail.com>
2020-05-12 14:24:40 +0200

    Dodano ITablicaOsobPienieznaKoncowaPrefabrykator.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/TablicaOsobKoncowaPrefabrykator/ITablicaOsobPienieznaKoncowaPrefabrykator.cs
@@ -0,0 +1,15 @@
+﻿using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.Interfejsy.TablicaOsobKoncowaPrefabrykator
+{
+    /// <summary>
+    /// Interfejs dla prefabrykatora tworzaczego tablice osob jaka jest oczekiwana pod koniec wyrownywania naleznosci.
+    /// </summary>
+    internal interface ITablicaOsobPienieznaKoncowaPrefabrykator
+    {
+        /// <summary>
+        /// Tworzy tablice osob jaka jest oczekiwana pod koniec wyrownywania naleznosci.
+        /// </summary>
+        ITablicaOsobPienieznych StworzTablicaKoncowa(ITablicaOsobPienieznychZwrotna tablicaOsob);
+    }
+}
