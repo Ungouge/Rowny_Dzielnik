2a23a3668dcc643b5fbcea3b1c02020965835bd8
Ungouge <zlobik89@gmail.com>
2020-05-12 15:06:43 +0200

    Dodano NajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz.

---
+++ Model/Dzielnik.Model/NajwiekszaMozliwaNaleznoscDoWymianyUstalacz/NajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz.cs
@@ -0,0 +1,41 @@
+﻿using Dzielnik.Model.Interfejsy.NajwiekszaMozliwaNaleznoscDoWymianyUstalacz;
+using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.Osoby;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.NajwiekszaMozliwaNaleznoscDoWymianyUstalacz
+{
+    /// <summary>
+    /// Ustalacza najwiekszej mozliwej naleznosci pienieznej jaka mozna wymienic pomiedzy osobami.
+    /// </summary>
+    internal class NajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz : INajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz
+    {
+        private readonly ITablicaOsobPienieznychZwrotna koncowaTablicaOsob;
+
+        private readonly INaleznoscPieniezna naleznoscPienieznaZerowa;
+
+        public NajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz(IFabrykaNaleznoscPienieznaZerowa fabrykaNaleznoscPienieznaZerowa,
+            ITablicaOsobPienieznychZwrotna koncowaTablicaOsob)
+        {
+            this.koncowaTablicaOsob = koncowaTablicaOsob;
+
+            naleznoscPienieznaZerowa = fabrykaNaleznoscPienieznaZerowa.StworzNaleznoscZerowa();
+        }
+
+        public INaleznoscPieniezna UstalNaleznoscDoWymiany(ITablicaOsobPienieznychZwrotna tablicaOsob, IOsobaID osobaKorzen, IOsobaID osobaPotomek)
+        {
+            INaleznoscPieniezna roznicaNaleznosciKorzenia =
+                tablicaOsob[osobaKorzen.ID].Wplata.Roznica(koncowaTablicaOsob[osobaKorzen.ID].Wplata);
+
+            INaleznoscPieniezna roznicaNaleznosciPotomka =
+                koncowaTablicaOsob[osobaPotomek.ID].Wplata.Roznica(tablicaOsob[osobaPotomek.ID].Wplata);
+
+            if (naleznoscPienieznaZerowa.CzyWieksze(roznicaNaleznosciPotomka))
+                if (naleznoscPienieznaZerowa.CzyWieksze(roznicaNaleznosciKorzenia))
+                    return roznicaNaleznosciKorzenia.CzyWieksze(roznicaNaleznosciPotomka) ? roznicaNaleznosciKorzenia : roznicaNaleznosciPotomka;
+            
+            return naleznoscPienieznaZerowa;
+        }
+    }
+}
