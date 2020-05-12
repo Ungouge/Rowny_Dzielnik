4570de50e83d03840407bc4e0b1fc40b91e23dee
Ungouge <zlobik89@gmail.com>
2020-05-12 15:06:29 +0200

    Dodano INajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/NajwiekszaMozliwaNaleznoscDoWymianyUstalacz/INajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz.cs
@@ -0,0 +1,17 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.Osoby;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.Interfejsy.NajwiekszaMozliwaNaleznoscDoWymianyUstalacz
+{
+    /// <summary>
+    /// Interfejs dla ustalacza najwiekszej mozliwej naleznosci pienieznej jaka mozna wymienic pomiedzy osobami.
+    /// </summary>
+    internal interface INajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz
+    {
+        /// <summary>
+        /// Ustal najwieksza mozliwa naleznosc pieniezna jaka mozna wymienic pomiedzy osobami.
+        /// </summary>
+        INaleznoscPieniezna UstalNaleznoscDoWymiany(ITablicaOsobPienieznychZwrotna tablicaOsob, IOsobaID osobaKorzen, IOsobaID osobaPotomek);
+    }
+}
