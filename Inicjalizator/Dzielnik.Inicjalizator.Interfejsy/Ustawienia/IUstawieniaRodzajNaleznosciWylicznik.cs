608e9dcaf7c8b3dd1cf568f6c5865b4a70b9b3f8
Ungouge <zlobik89@gmail.com>
2020-05-14 22:59:27 +0200

    Dodano IUstawieniaRodzajNaleznosciWylicznik.

---
+++ Inicjalizator/Dzielnik.Inicjalizator.Interfejsy/Ustawienia/IUstawieniaRodzajNaleznosciWylicznik.cs
@@ -0,0 +1,15 @@
+﻿using Dzielnik.Zasoby.Wyliczniki;
+
+namespace Dzielnik.Inicjalizator.Interfejsy
+{
+    /// <summary>
+    /// Interfejs dla klasy z ustawieniami programu z rodzajem naleznosci.
+    /// </summary>
+    public interface IUstawieniaRodzajNaleznosciWylicznik
+    {
+        /// <summary>
+        /// Wez roddzaj naleznosci obesnie ustawiony w programie.
+        /// </summary>
+        RodzajNaleznosciWylicznik RodzajNaleznosciWylicznik { get; }
+    }
+}
