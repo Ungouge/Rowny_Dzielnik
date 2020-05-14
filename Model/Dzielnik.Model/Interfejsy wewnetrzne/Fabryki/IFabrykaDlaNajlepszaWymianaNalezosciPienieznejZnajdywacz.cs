6096b5846ccfaa37e4cb0310a9f6859fbb78254f
Ungouge <zlobik89@gmail.com>
2020-05-14 20:53:59 +0200

    Dodano IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz.cs
@@ -0,0 +1,24 @@
+﻿using Dzielnik.Model.Interfejsy.DoStanuKwitaSumator;
+using Dzielnik.Model.Interfejsy.NajwiekszaMozliwaNaleznoscDoWymianyUstalacz;
+using Dzielnik.Model.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla fabryki do tworzenia potrzebnych obiektow dla NajlepszaWymianaNalezosciPienieznejZnajdywacz.
+    /// </summary>
+    internal interface IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz
+    {
+        IRozniceDoStanuKwitaNaleznosciPienieznychSumator StworzRozniceDoStanuKwitaNaleznosciPienieznychSumator(ITablicaOsobPienieznychIterowalna tablicaOsobPoczatkowa);
+
+        INajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz StworzNajwiekszaMozliwaNaleznoscDoWymianyUstalacz(ITablicaOsobPienieznychZwrotna poczatkowaTablicaOsob);
+
+        ITabelaNaleznosciPienieznej StworzStartowaTabelaNaleznosciPienieznej();
+
+        ITabelaNaleznosciPienieznej StworzTabelaNaleznosciPienieznejNull();
+
+        IPolozenieWTabeliNaleznosci StworzPolozenieWTabeliNaleznosci(byte index_X, byte index_Y);
+    }
+}
