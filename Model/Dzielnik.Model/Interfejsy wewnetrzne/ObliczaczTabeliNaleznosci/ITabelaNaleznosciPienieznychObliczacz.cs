595dea3b82aaec2cccd5d458019d0dfc251a9688
Ungouge <zlobik89@gmail.com>
2020-05-14 23:27:10 +0200

    Dodano ITabelaNaleznosciPienieznychObliczacz.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/ObliczaczTabeliNaleznosci/ITabelaNaleznosciPienieznychObliczacz.cs
@@ -0,0 +1,16 @@
+﻿using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.Interfejsy.ObliczaczTabeliNaleznosci
+{
+    /// <summary>
+    /// Interfejs dla obliczacza tabeli nalezcnosci pienieznych.
+    /// </summary>
+    internal interface ITabelaNaleznosciPienieznychObliczacz : ITabelaNaleznosciObliczacz
+    {
+        /// <summary>
+        /// Zwraca obliczone naleznosci dla podanej tabeli osob i obecnych ustawien.
+        /// </summary>
+        ITabelaNaleznosciPienieznejZwrotna ObliczNaleznosci(ITablicaOsobPienieznych tablicaOsob);
+    }
+}
