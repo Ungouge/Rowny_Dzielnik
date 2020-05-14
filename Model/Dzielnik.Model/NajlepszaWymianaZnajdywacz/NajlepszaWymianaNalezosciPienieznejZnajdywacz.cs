1e425e4e7f4f0bbfe09b05de65da13c58ba50bb7
Ungouge <zlobik89@gmail.com>
2020-05-14 20:54:13 +0200

    Dodano NajlepszaWymianaNalezosciPienieznejZnajdywacz.

---
+++ Model/Dzielnik.Model/NajlepszaWymianaZnajdywacz/NajlepszaWymianaNalezosciPienieznejZnajdywacz.cs
@@ -0,0 +1,30 @@
+﻿using Dzielnik.Model.Interfejsy.DoStanuKwitaSumator;
+using Dzielnik.Model.Interfejsy.Fabryki;
+using Dzielnik.Model.Interfejsy.NajlepszaWymianaZnajdywacz;
+using Dzielnik.Model.Interfejsy.NajwiekszaMozliwaNaleznoscDoWymianyUstalacz;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.NajlepszaWymianaZnajdywacz
+{
+    /// <summary>
+    /// Znajdywacza najlepszej wymiany naleznosci pienieznej.
+    /// </summary>
+    internal partial class NajlepszaWymianaNalezosciPienieznejZnajdywacz : INajlepszaWymianaNalezosciPienieznejZnajdywacz
+    {
+        private readonly IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz fabryka;
+
+        private readonly INajwiekszaMozliwaNaleznoscPienieznaDoWymianyUstalacz najwiekszaMozliwaNaleznoscDoWymianyUstalacz;
+
+        private readonly IRozniceDoStanuKwitaNaleznosciPienieznychSumator rozniceDoStanuKwitaSumator;
+
+        internal NajlepszaWymianaNalezosciPienieznejZnajdywacz(IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz fabryka,
+            ITablicaOsobPienieznych tablicaOsobKoncowa)
+        {
+            this.fabryka = fabryka;
+
+            najwiekszaMozliwaNaleznoscDoWymianyUstalacz = this.fabryka.StworzNajwiekszaMozliwaNaleznoscDoWymianyUstalacz(tablicaOsobKoncowa);
+
+            rozniceDoStanuKwitaSumator = this.fabryka.StworzRozniceDoStanuKwitaNaleznosciPienieznychSumator(tablicaOsobKoncowa);
+        }
+    }
+}
---
+++ Model/Dzielnik.Model/NajlepszaWymianaZnajdywacz/Znajdz.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+using Dzielnik.Model.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.Osoby;
+using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.NajlepszaWymianaZnajdywacz
+{
+    partial class NajlepszaWymianaNalezosciPienieznejZnajdywacz
+    {
+        public ITabelaNaleznosciPienieznejZwrotna Znajdz(ITablicaOsobPienieznych tablicaOsob)
+        {
+            return Znajdz(tablicaOsob, rozniceDoStanuKwitaSumator.SumujRoznice(tablicaOsob));
+        }
+
+        private ITabelaNaleznosciPienieznej Znajdz(ITablicaOsobPienieznych tablicaOsob, long staraRozniceDoPoczatkowej)
+        {
+            ITabelaNaleznosciPienieznej najlepszaTabelaNaleznosci = fabryka.StworzTabelaNaleznosciPienieznejNull();
+
+            foreach (IOsobaPieniezna osobaKorzen in (tablicaOsob as IEnumerable<IOsobaPieniezna>))
+            {
+                foreach (IOsobaPieniezna osobaPotomek in tablicaOsob)
+                {
+                    INaleznoscPieniezna roznicaNaleznosci = najwiekszaMozliwaNaleznoscDoWymianyUstalacz.UstalNaleznoscDoWymiany(tablicaOsob, osobaKorzen, osobaPotomek);
+
+                    if (roznicaNaleznosci.Swiadczenie == 0)
+                        continue;
+
+                    ITablicaOsobPienieznych nowaTablicaOsob = tablicaOsob.PrzekazNaleznosciPomiedzyOsobami(osobaKorzen, osobaPotomek, roznicaNaleznosci);
+
+                    long rozniceDoPoczatkowej = rozniceDoStanuKwitaSumator.SumujRoznice(nowaTablicaOsob);
+
+                    if (rozniceDoPoczatkowej >= staraRozniceDoPoczatkowej)
+                        continue;
+
+                    ITabelaNaleznosciPienieznej NowaTabelaNaleznosci = ZnajdzTabeleNaleznosciDlaNoweTablicyOsob(nowaTablicaOsob, rozniceDoPoczatkowej);
+
+                    NowaTabelaNaleznosci.DodajWymianeNalzenosciUaktualniajacKryterium(roznicaNaleznosci,
+                        fabryka.StworzPolozenieWTabeliNaleznosci(osobaPotomek.ID, osobaKorzen.ID));
+
+                    najlepszaTabelaNaleznosci = najlepszaTabelaNaleznosci.ZwrocLepszeKryterium( NowaTabelaNaleznosci);
+                }
+            }
+
+            return najlepszaTabelaNaleznosci;
+        }
+
+        private ITabelaNaleznosciPienieznej ZnajdzTabeleNaleznosciDlaNoweTablicyOsob(ITablicaOsobPienieznych nowaTablicaOsob, long rozniceDoPoczatkowej)
+        {
+            if (rozniceDoPoczatkowej == 0)
+                return fabryka.StworzStartowaTabelaNaleznosciPienieznej();
+
+            return Znajdz(nowaTablicaOsob, rozniceDoPoczatkowej);
+        }
+    }
+}
