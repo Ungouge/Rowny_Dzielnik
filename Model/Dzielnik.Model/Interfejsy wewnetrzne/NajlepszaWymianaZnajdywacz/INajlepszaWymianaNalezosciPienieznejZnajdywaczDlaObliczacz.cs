055fc6c3a5a0265da0a61f606c9f686355712ef6
Ungouge <zlobik89@gmail.com>
2020-05-14 20:48:55 +0200

    Dodadno INajlepszaWymianaNalezosciPienieznejZnajdywaczDlaObliczacz.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/NajlepszaWymianaZnajdywacz/INajlepszaWymianaNalezosciPienieznejZnajdywaczDlaObliczacz.cs
@@ -0,0 +1,16 @@
+﻿using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.Interfejsy.NajlepszaWymianaZnajdywacz
+{
+    /// <summary>
+    /// Interfejs dla znajdywacza najlepszej wymiany naleznosci pienieznej przeznaczonej dla obliczacza tabeli naleznosci.
+    /// </summary>
+    internal interface INajlepszaWymianaNalezosciPienieznejZnajdywaczDlaObliczacz
+    {
+        /// <summary>
+        /// Zwraca znaleziona najlepsza wymiane naleznosci pienieznej wedlug obenie ustawionego kryterium wyboru.
+        /// </summary>
+        ITabelaNaleznosciPienieznejZwrotna Znajdz(ITablicaOsobPienieznych tablicaOsob);
+    }
+}
