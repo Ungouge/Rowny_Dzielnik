7c0641ab21950c4474ca885c9e969dbc724466bd
Ungouge <zlobik89@gmail.com>
2020-05-16 00:50:53 +0200

    Dodadno IFabrykaDlaObliczaczTabeliNaleznosciProkurent.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/IFabrykaDlaObliczaczTabeliNaleznosciProkurent.cs
@@ -0,0 +1,12 @@
+﻿using Dzielnik.Model.Interfejsy.ObliczaczTabeliNaleznosci;
+
+namespace Dzielnik.Model.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla fabryki do tworzenia potrzebnych obiektow dla ObliczaczTabeliNaleznosciProkurent.
+    /// </summary>
+    internal interface IFabrykaDlaObliczaczTabeliNaleznosciProkurent
+    {
+        ITabelaNaleznosciPienieznychObliczacz StworzObliczaczTabeliNaleznosciPienieznych();
+    }
+}
