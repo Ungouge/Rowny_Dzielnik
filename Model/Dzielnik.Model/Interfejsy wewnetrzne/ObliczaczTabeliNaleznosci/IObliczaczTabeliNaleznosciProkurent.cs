9e2400d4fb870ca57030f0345bd9b53e74f926bd
Ungouge <zlobik89@gmail.com>
2020-05-16 00:49:34 +0200

    Dodano IObliczaczTabeliNaleznosciProkurent.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/ObliczaczTabeliNaleznosci/IObliczaczTabeliNaleznosciProkurent.cs
@@ -0,0 +1,13 @@
+﻿using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.Interfejsy.ObliczaczTabeliNaleznosci
+{
+    /// <summary>
+    /// Interfejs dla prokurenta wybierajacego odpowiedni obliczacz tabeli naleznosci w zaleznosci od obecnych ustawien.
+    /// </summary>
+    internal interface IObliczaczTabeliNaleznosciProkurent
+    {
+        ITabelaNaleznosciZwrotna ObliczNaleznosci(ITablicaOsob tablicaOsob);
+    }
+}
