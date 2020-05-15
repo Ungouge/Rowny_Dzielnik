d36c622edacd423883e9a7b383a6b51d79951d29
Ungouge <zlobik89@gmail.com>
2020-05-15 23:12:27 +0200

    Doadno IFabrykaDlaTabelaNaleznosciObliczacz.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/dlaTabelaNaleznosciObliczacz/IFabrykaDlaTabelaNaleznosciObliczacz.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Dzielnik.Model.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs zbiorczy dla fabryk do obliczaczy tabel naleznosci.
+    /// </summary>
+    internal interface IFabrykaDlaTabelaNaleznosciObliczacz : IFabrykaDlaTabelaNaleznosciPienieznychObliczacz
+    {
+    }
+}
