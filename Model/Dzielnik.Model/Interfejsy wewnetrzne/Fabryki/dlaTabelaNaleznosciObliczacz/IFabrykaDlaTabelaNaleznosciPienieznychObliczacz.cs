37411445e869d591a260dae0274aebdd4894394b
Ungouge <zlobik89@gmail.com>
2020-05-15 23:10:10 +0200

    Dodano IFabrykaDlaTabelaNaleznosciPienieznychObliczacz.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/dlaTabelaNaleznosciObliczacz/IFabrykaDlaTabelaNaleznosciPienieznychObliczacz.cs
@@ -0,0 +1,22 @@
+﻿using Dzielnik.Model.Interfejsy.NajlepszaWymianaZnajdywacz;
+using Dzielnik.Model.Interfejsy.TablicaOsobKoncowaPrefabrykator;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.Model.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla fabryk do obliczaczy tabel naleznosci pienieznych.
+    /// </summary>
+    internal interface IFabrykaDlaTabelaNaleznosciPienieznychObliczacz
+    {
+        /// <summary>
+        /// Tworzy instancje znajdywacza najlepszej wymiany naleznosci pienieznej.
+        /// </summary>
+        INajlepszaWymianaNalezosciPienieznejZnajdywacz StworzNajlepszaWymianaNalezosciPienieznejZnajdywacz(ITablicaOsobPienieznych tablicaOsobKoncowa);
+
+        /// <summary>
+        /// Tworzy instancje prefabrykatora tablic osob pienieznych.
+        /// </summary>
+        ITablicaOsobPienieznaKoncowaPrefabrykator StworzTablicaOsobPienieznaKoncowaPrefabrykator();
+    }
+}
