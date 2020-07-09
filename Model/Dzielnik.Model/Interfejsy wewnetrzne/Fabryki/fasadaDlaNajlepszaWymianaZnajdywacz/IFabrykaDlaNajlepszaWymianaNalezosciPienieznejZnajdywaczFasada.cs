3e8abb65674d04707caa4f76c2744a3e5b612083
Ungouge <zlobik89@gmail.com>
2020-07-10 00:00:37 +0200

    Dodano IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywaczFasada.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/fasadaDlaNajlepszaWymianaZnajdywacz/IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywaczFasada.cs
@@ -0,0 +1,17 @@
+﻿using Dzielnik.Model.Interfejsy.TabeleNaleznosci;
+using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+
+namespace Dzielnik.Model.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla fasady fabryki do tworzenia potrzebnych obiektow dla NajlepszaWymianaNalezosciPienieznejZnajdywacz.
+    /// </summary>
+    internal interface IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywaczFasada
+    {
+        ITabelaNaleznosciPienieznej StworzStartowaTabelaNaleznosciPienieznej();
+
+        ITabelaNaleznosciPienieznej StworzTabelaNaleznosciPienieznejNull();
+
+        IPolozenieWTabeliNaleznosci StworzPolozenieWTabeliNaleznosci(byte index_X, byte index_Y);
+    }
+}
