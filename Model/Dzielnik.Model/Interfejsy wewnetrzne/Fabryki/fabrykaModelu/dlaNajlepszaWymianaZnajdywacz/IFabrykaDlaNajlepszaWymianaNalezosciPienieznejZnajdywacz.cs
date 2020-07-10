02f9ff2d5bd11ab901bca7ebb1d8c310813776a4
Ungouge <zlobik89@gmail.com>
2020-07-10 08:38:33 +0200

    Czyszczenie kodu oraz uzupelnienie pominietego opisu

--- Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/fabrykaModelu/dlaNajlepszaWymianaZnajdywacz/IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz.cs
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/fabrykaModelu/dlaNajlepszaWymianaZnajdywacz/IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz.cs
@@ -1,9 +1,6 @@
-﻿using Dzielnik.Model.Interfejsy.DoStanuKwitaSumator;
-using Dzielnik.Model.Interfejsy.NajwiekszaMozliwaNaleznoscDoWymianyUstalacz;
-using Dzielnik.Model.Interfejsy.TabeleNaleznosci;
+﻿using Dzielnik.Model.Interfejsy.TabeleNaleznosci;
 using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
 using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
-using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
 
 namespace Dzielnik.Model.Interfejsy.Fabryki
 {
@@ -12,10 +9,19 @@ namespace Dzielnik.Model.Interfejsy.Fabryki
     /// </summary>
     internal interface IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz
     {
+        /// <summary>
+        /// Stworz instancje ITabelaNaleznosciPienieznej dla podanych argumentow.
+        /// </summary>
         ITabelaNaleznosciPienieznej StworzStartowaTabelaNaleznosciPienieznej(byte iloscOsob, KryteriumWyboruTabeliNaleznosciWylicznik kryteriumWyboruTabeliNaleznosci);
 
+        /// <summary>
+        /// Tworzy instancje ITabelaNaleznosciPienieznej z zachowaniem pustym dla podanego argument.
+        /// </summary>
         ITabelaNaleznosciPienieznej StworzTabelaNaleznosciPienieznejNull(byte iloscOsob);
-
+        
+        /// <summary>
+        /// Tworzy instancje PolozenieWTabeliNaleznosci dla podanych koordynatow.
+        /// </summary>
         IPolozenieWTabeliNaleznosci StworzPolozenieWTabeliNaleznosci(byte index_X, byte index_Y);
     }
 }
--- Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/fasadaDlaNajlepszaWymianaZnajdywacz/IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywaczFasada.cs
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/fasadaDlaNajlepszaWymianaZnajdywacz/IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywaczFasada.cs
@@ -8,10 +8,19 @@ namespace Dzielnik.Model.Interfejsy.Fabryki
     /// </summary>
     internal interface IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywaczFasada
     {
+        /// <summary>
+        /// Tworzy instancje ITabelaNaleznosciPienieznej.
+        /// </summary>
         ITabelaNaleznosciPienieznej StworzStartowaTabelaNaleznosciPienieznej();
 
+        /// <summary>
+        /// Tworzy instancje ITabelaNaleznosciPienieznej z zachowaniem pustym.
+        /// </summary>
         ITabelaNaleznosciPienieznej StworzTabelaNaleznosciPienieznejNull();
 
+        /// <summary>
+        /// Tworzy instancje PolozenieWTabeliNaleznosci dla podanych koordynatow.
+        /// </summary>
         IPolozenieWTabeliNaleznosci StworzPolozenieWTabeliNaleznosci(byte index_X, byte index_Y);
     }
 }
