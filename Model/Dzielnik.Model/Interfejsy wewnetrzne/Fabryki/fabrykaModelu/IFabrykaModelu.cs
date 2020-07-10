5ab48b02d28a2ab9671333b0a5f695614d15cfe1
Ungouge <zlobik89@gmail.com>
2020-07-10 08:41:06 +0200

    Dodano IFabrykaModelu.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/fabrykaModelu/IFabrykaModelu.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Dzielnik.Model.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla fabryki do tworzenia potrzebnych obiektow w Modelu.
+    /// </summary>
+    interface IFabrykaModelu : IFabrykaDlaObliczaczTabeliNaleznosciProkurent, IFabrykaDlaTabelaNaleznosciObliczacz,
+        IFabrykaDlaNajlepszaWymianaNalezosciPienieznejZnajdywacz, IFabrykaDlaModelu
+    {
+    }
+}
