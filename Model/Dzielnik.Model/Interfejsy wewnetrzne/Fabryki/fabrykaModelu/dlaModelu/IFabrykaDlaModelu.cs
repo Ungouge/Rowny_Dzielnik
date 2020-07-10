ce20ab6d9518b4885c26c057875ea86cab0045c2
Ungouge <zlobik89@gmail.com>
2020-07-10 08:40:55 +0200

    Dodadno IFabrykaDlaModelu.

---
+++ Model/Dzielnik.Model/Interfejsy wewnetrzne/Fabryki/fabrykaModelu/dlaModelu/IFabrykaDlaModelu.cs
@@ -0,0 +1,16 @@
+﻿
+using Dzielnik.Model.Interfejsy.ObliczaczTabeliNaleznosci;
+
+namespace Dzielnik.Model.Interfejsy.Fabryki
+{
+    /// <summary>
+    /// Interfejs dla fabryki do tworzenia potrzebnych obiektow dla ModelDzielnika.
+    /// </summary>
+    interface IFabrykaDlaModelu
+    {
+        /// <summary>
+        /// Stworz instancje ObliczaczTabeliNaleznosciProkurent.
+        /// </summary>
+        IObliczaczTabeliNaleznosciProkurent StworzObliczaczTabeliNaleznosciProkurent();
+    }
+}
