4c153816685bc200e174ec182305f3c696de746f
Ungouge <zlobik89@gmail.com>
2020-07-10 09:08:58 +0200

    Dodano interfejsy inicjalizatora modelu.

---
+++ Model/Dzielnik.Model.Interfejsy/Inicjalizator/IInicjaliztorDlaFabrykaModelu.cs
@@ -0,0 +1,13 @@
+﻿using Dzielnik.Inicjalizator.Interfejsy;
+using Dzielnik.Zasoby.Interfejsy.Fabryki;
+
+namespace Dzielnik.Model.Interfejsy.Inicjalizator
+{
+    /// <summary>
+    /// Interfejs inicjalizatora fabryki modelu.
+    /// </summary>
+    public interface IInicjaliztorDlaFabrykaModelu :IInicjalizatorFabrykaNaleznosci, IInicjalizatorFabrykaTablicaOsob
+    {
+        IFabrykaPolozeniaWTabeliNaleznosci FabrykaPolozeniaWTabeliNaleznosci { get; }
+    }
+}
---
+++ Model/Dzielnik.Model.Interfejsy/Inicjalizator/IInicjaliztorDlaModelu.cs
@@ -0,0 +1,16 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Fabryki;
+
+namespace Dzielnik.Model.Interfejsy.Inicjalizator
+{
+    /// <summary>
+    /// Interfejs inicjalizotora modelu.
+    /// </summary>
+    public interface IInicjaliztorDlaModelu : IInicjaliztorDlaFabrykaModelu
+    {
+        IAbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych AbstrakcyjnaFabrykaKryteriumWyboruTabeliNaleznosciPienieznych { get; }
+
+        IAbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci AbstrakcyjnaFabrykaNaleznosciPienieznaDlaTabeliNaleznosci { get; }
+
+        IFabrykaKryteriumWyboruTabeliNaleznosciPienieznychNull FabrykaKryteriumWyboruTabeliNaleznosciNull { get; }
+    }
+}
