7b0005fa4c48ab973d61d53e984d0d655cb21b88
Ungouge <zlobik89@gmail.com>
2020-07-15 15:50:44 +0200

    Dodano ObliczonaTabelaNaleznosci_ArgumentZdarzenia; zmieniono dostosowana obsluge zdarzenia obliczonej tabeli naleznosci na obsluge zdarzenia z dostosowanym argumentem.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Zdarzenia/ObliczonaTabelaNaleznosci_ArgumentZdarzenia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using Dzielnik.Zasoby.Interfejsy.TabeleNaleznosci;
+
+namespace Dzielnik.Zasoby.Zdarzenia
+{
+    /// <summary>
+    /// Argument zdarzenie dla obslugi zdarzenia notyfikacji obliczenie nowej tabeli naleznosci przez model.
+    /// </summary>
+    [ComVisible(true)]
+    public class ObliczonaTabelaNaleznosci_ArgumentZdarzenia: EventArgs
+    {
+        public ITabelaNaleznosciZwrotna TabelaNaleznosciZwrotna { get; }
+        
+        private static ObliczonaTabelaNaleznosci_ArgumentZdarzenia WezPusty =>
+            new ObliczonaTabelaNaleznosci_ArgumentZdarzenia(null);
+
+        public static new readonly ObliczonaTabelaNaleznosci_ArgumentZdarzenia Empty = WezPusty;
+
+        /// <summary>
+        /// Stworz argument zdarzenia dla obliczonej tabeli naleznosci.
+        /// </summary>
+        public ObliczonaTabelaNaleznosci_ArgumentZdarzenia(ITabelaNaleznosciZwrotna tabelaNaleznosciZwrotna)
+        {
+            TabelaNaleznosciZwrotna = tabelaNaleznosciZwrotna;
+        }
+    }
+}
