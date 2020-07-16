f61fccb8213d715f25973bae960f5f2ee0a1251b
Ungouge <zlobik89@gmail.com>
2020-07-16 23:42:53 +0200

    Dodano INaleznoscNaStringKonwerter.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Interfejsy wewnetrzne/Konwertery/naleznoscNaString/INaleznoscNaStringKonwerter.cs
@@ -0,0 +1,20 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+namespace Dzielnik.ModelWidoku.Interfejsy.Konwertery
+{
+    /// <summary>
+    /// Interfejs dla konwertera przeslanej naleznosci na tekstu w odpowiedni dla implementacji sposob.
+    /// </summary>
+    internal interface INaleznoscNaStringKonwerter
+    {
+        /// <summary>
+        /// Konwertuj przeslana naleznosc na tekstu w odpowiedni dla implementacji sposob.
+        /// </summary>
+        string NaString(INaleznoscSwiadczenieBaza naleznosc);
+
+        /// <summary>
+        /// Konwertuj odwrotnosc przeslanej naleznosc na tekstu w odpowiedni dla implementacji sposob.
+        /// </summary>
+        string NaStringOdwrotnosc(INaleznoscSwiadczenieBaza naleznosc);
+    }
+}
