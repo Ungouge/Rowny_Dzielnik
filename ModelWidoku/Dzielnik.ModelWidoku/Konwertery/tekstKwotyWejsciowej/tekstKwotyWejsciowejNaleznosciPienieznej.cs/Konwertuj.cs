d64fa514ddda19a90d8117c2063a1fbaf8d0b7c0
Ungouge <zlobik89@gmail.com>
2020-07-16 23:14:04 +0200

    Dodano TekstKwotyWejsciowejNaleznosciPienieznejKonwerter.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Konwertery/tekstKwotyWejsciowej/tekstKwotyWejsciowejNaleznosciPienieznej.cs/Konwertuj.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+namespace Dzielnik.ModelWidoku.Konwertery
+{
+    partial class TekstKwotyWejsciowejNaleznosciPienieznejKonwerter
+    {
+        public bool Konwertuj(string tekstKwotyWejsciowej, out INaleznoscSwiadczenieBaza naleznosc)
+        {
+            bool walidacjaFlaga = KonwertujNaZmiennoprzcinkowy(tekstKwotyWejsciowej, out float swiadczeniePieniezne);
+
+            naleznosc = WezNaleznoscDlaSwiadczenia(swiadczeniePieniezne);
+
+            return walidacjaFlaga;
+        }
+
+        private static bool KonwertujNaZmiennoprzcinkowy(string tekstKwotyWejsciowej, out float swiadczeniePieniezne)
+        {
+            if (String.IsNullOrEmpty(tekstKwotyWejsciowej))
+            {
+                swiadczeniePieniezne = 0;
+
+                return true;
+            }
+
+            return float.TryParse(tekstKwotyWejsciowej, out swiadczeniePieniezne);
+        }
+
+        private INaleznoscSwiadczenieBaza WezNaleznoscDlaSwiadczenia(float swiadczeniePieniezne)
+        {
+            if (swiadczeniePieniezne == 0)
+                return naleznoscZerowa;
+
+            return fabryka.StworzNaleznosc((int)Math.Round(1e2 * swiadczeniePieniezne));
+        }
+    }
+}
---
+++ ModelWidoku/Dzielnik.ModelWidoku/Konwertery/tekstKwotyWejsciowej/tekstKwotyWejsciowejNaleznosciPienieznej.cs/TekstKwotyWejsciowejNaleznosciPienieznejKonwerter.cs
@@ -0,0 +1,26 @@
+﻿using Dzielnik.ModelWidoku.Interfejsy.Konwertery;
+using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+namespace Dzielnik.ModelWidoku.Konwertery
+{
+    /// <summary>
+    /// Konwerter tekstu z komorki kwoty wejsciowe na odpowiedni typ naleznosci pienieznej.
+    /// </summary>
+    internal partial class TekstKwotyWejsciowejNaleznosciPienieznejKonwerter : ITekstKwotyWejsciowejKonwerter
+    {
+        private IFabrykaNaleznoscPieniezna fabryka;
+
+        private readonly INaleznoscPieniezna naleznoscZerowa;
+
+        /// <summary>
+        /// Stworz konwerter tekstu z komorki kwoty wejsciowe na odpowiedni typ nalznosci pienieznej.
+        /// </summary>
+        public TekstKwotyWejsciowejNaleznosciPienieznejKonwerter( IFabrykaNaleznoscPieniezna fabryka, INaleznoscPieniezna naleznoscZerowa)
+        {
+            this.fabryka = fabryka;
+
+            this.naleznoscZerowa = naleznoscZerowa;
+        }
+    }
+}
