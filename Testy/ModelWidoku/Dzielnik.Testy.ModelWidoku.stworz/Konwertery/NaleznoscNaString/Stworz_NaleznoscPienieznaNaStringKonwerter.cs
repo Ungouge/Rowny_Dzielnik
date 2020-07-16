421fe56b447bed330d74088a4a30db5e09132a73
Ungouge <zlobik89@gmail.com>
2020-07-16 23:54:06 +0200

    Dodano Czy_NaleznoscNaStringProkurent_NaString; oraz pomocnicze klasy do tworzenia potrzebnych testowalnych obiektow i atrap.

---
+++ Testy/ModelWidoku/Dzielnik.Testy.ModelWidoku.stworz/Konwertery/NaleznoscNaString/Stworz_NaleznoscPienieznaNaStringKonwerter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dzielnik.ModelWidoku.Interfejsy.Konwertery;
+using Dzielnik.ModelWidoku.Konwertery;
+using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+namespace Dzielnik.Testy.ModelWidoku.Konwertery.NaleznoscNaString
+{
+    internal static class Stworz_NaleznoscPienieznaNaStringKonwerter
+    {
+        internal static INaleznoscNaStringKonwerter Stworz()
+        {
+            return new NaleznoscPienieznaNaStringKonwerter();
+        }
+    }
+}
---
+++ Testy/Zasoby/Dzielnik.Testy.Zasoby.stworzMocki/Naleznosci/Stworz_NaleznoscSwiadczenieBaza_Mock.cs
@@ -0,0 +1,14 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+using Moq;
+
+namespace Dzielnik.Testy.Zasoby.Naleznosci
+{
+    public static class Stworz_NaleznoscSwiadczenieBaza_Mock
+    {
+        public static INaleznoscSwiadczenieBaza Stworz_Mock()
+        {
+            return new Mock<INaleznoscSwiadczenieBaza>().Object;
+        }
+    }
+}
