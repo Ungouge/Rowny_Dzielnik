67128eed6da5b836958b1273a9e0ab1fda414591
Ungouge <zlobik89@gmail.com>
2020-07-16 21:48:48 +0200

    Dodano ModelUaktualniacz.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Uaktualniacz/ModelUaktualniacz.cs
@@ -0,0 +1,28 @@
+﻿using Dzielnik.Model.Interfejsy;
+using Dzielnik.ModelWidoku.Interfejsy.Uaktualniacz;
+using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.ModelWidoku.Uaktualniacz
+{
+    /// <summary>
+    /// Uaktualniacz przenaczony dla uaktualniania modelu.
+    /// </summary>
+    internal class ModelUaktualniacz : IModelUaktualniacz
+    {
+        private readonly IModelDzielnikaDlaUaktualniaczModelu model;
+
+        /// <summary>
+        /// Stworz ukatalniacz przeslanego modelu.
+        /// </summary>
+        public ModelUaktualniacz(IModelDzielnikaDlaUaktualniaczModelu model)
+        {
+            this.model = model;
+        }
+        
+        public void Uaktualnij(ITablicaOsobBaza tablicaOsob, KryteriumWyboruTabeliNaleznosciWylicznik kryteriumWyboruTabeliNaleznosci)
+        {
+            model.Uaktualnij(tablicaOsob, kryteriumWyboruTabeliNaleznosci);
+        }
+    }
+}
