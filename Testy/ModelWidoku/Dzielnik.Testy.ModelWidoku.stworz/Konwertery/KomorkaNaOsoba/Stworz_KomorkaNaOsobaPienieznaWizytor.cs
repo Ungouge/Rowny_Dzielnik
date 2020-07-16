8fc81e130af425d34c98de13dab26e05b0ee10c9
Ungouge <zlobik89@gmail.com>
2020-07-16 23:10:29 +0200

    Dodano Czy_KomorkaNaOsobaPienieznaWizytor_CzyOdpowiadaRodzajowiNaleznosci; oraz pomocnicze klasy do tworzenia potrzebnych testowalnych obiektow.

---
+++ Testy/ModelWidoku/Dzielnik.Testy.ModelWidoku.stworz/Konwertery/KomorkaNaOsoba/Stworz_KomorkaNaOsobaPienieznaWizytor.cs
@@ -0,0 +1,18 @@
+﻿
+using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.ModelWidoku.Konwertery;
+using Dzielnik.ModelWidoku.Interfejsy.Konwertery;
+using Dzielnik.Zasoby.Fabryki;
+
+namespace Dzielnik.Testy.ModelWidoku.Konwertery.KomorkaNaOsoba
+{
+    internal static class Stworz_KomorkaNaOsobaPienieznaWizytor
+    {
+        private static readonly IFabrykaOsobaPieniezna fabrykaOsobaPieniezna = new FabrykaOsobaPieniezna();
+
+        internal static IKonwerterKomorkaNaOsobaWizytator Stworz()
+        {
+            return new KomorkaNaOsobaPienieznaWizytator(fabrykaOsobaPieniezna);
+        }
+    }
+}
---
+++ Testy/ModelWidoku/Dzielnik.Testy.ModelWidoku/Konwertery/KomorkaNaOsoba/komorkaNaOsobaPieniezna/Czy_KomorkaNaOsobaPienieznaWizytor_CzyOdpowiadaRodzajowiNaleznosci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Dzielnik.ModelWidoku.Interfejsy.Konwertery;
+using Dzielnik.Zasoby.Wyliczniki;
+
+using Xunit;
+
+namespace Dzielnik.Testy.ModelWidoku.Konwertery.KomorkaNaOsoba
+{
+    public class Czy_KomorkaNaOsobaPienieznaWizytor_CzyOdpowiadaRodzajowiNaleznosci
+    {
+        [Fact]
+        public void Czy_CzyOdpowiadaRodzajowiNaleznosci_Zwraca_Prawde()
+        {
+            //Ustal
+            IKonwerterKomorkaNaOsobaWizytator konwerter = Stworz_KomorkaNaOsobaPienieznaWizytor.Stworz();
+
+            //Dzialaj
+            bool czyOdpowiadaRodzajowiNaleznosci = konwerter.CzyOdpowiadaRodzajowiNaleznosci(RodzajNaleznosciWylicznik.NaleznoscPieniezna);
+
+            //Asercja
+            Assert.True(czyOdpowiadaRodzajowiNaleznosci);
+        }
+
+        [Fact]
+        public void Czy_CzyOdpowiadaRodzajowiNaleznosci_Zwraca_Falsz()
+        {
+            //Ustal
+            IKonwerterKomorkaNaOsobaWizytator konwerter = Stworz_KomorkaNaOsobaPienieznaWizytor.Stworz();
+
+            //Dzialaj
+            bool czyOdpowiadaRodzajowiNaleznosci = konwerter.CzyOdpowiadaRodzajowiNaleznosci((RodzajNaleznosciWylicznik) Int32.MaxValue);
+
+            //Asercja
+            Assert.False(czyOdpowiadaRodzajowiNaleznosci);
+        }
+    }
+}
