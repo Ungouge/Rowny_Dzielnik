ad393416c6a0e5adfb2992176eff77bea8838148
Ungouge <zlobik89@gmail.com>
2020-07-16 23:13:14 +0200

    Dodano IKonwerterTekstKwotyWejsciowejProkurent.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Interfejsy wewnetrzne/Konwertery/tekstKwotyWejsciowej/IKonwerterTekstKwotyWejsciowejProkurent.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Dzielnik.ModelWidoku.Interfejsy.Konwertery
+{
+    /// <summary>
+    /// Interfejs dla prokurenta konwerterow tekstu z komorki kwoty wejsciowe na odpowiedni typ nalznosci.
+    /// </summary>
+    internal interface IKonwerterTekstKwotyWejsciowejProkurent : IKonwerterTekstKwotyWejsciowejProkurentDlaKomorka
+    {
+        /// <summary>
+        /// Uaktualnij implentacje konwertera ktora bedzie prokurowana.
+        /// </summary>
+        void UaktualnijProkurent(ITekstKwotyWejsciowejKonwerter walidator);
+    }
+}
---
+++ ModelWidoku/Dzielnik.ModelWidoku/Interfejsy wewnetrzne/Konwertery/tekstKwotyWejsciowej/IKonwerterTekstKwotyWejsciowejProkurentDlaKomorka.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Dzielnik.ModelWidoku.Zdarzenia;
+
+namespace Dzielnik.ModelWidoku.Interfejsy.Konwertery
+{
+    /// <summary>
+    /// Interfejs dla prokurenta konwerterow tekstu z komorki kwoty wejsciowe na odpowiedni typ naleznosci przeznaczony dla komorek tabeli.
+    /// </summary>
+    internal interface IKonwerterTekstKwotyWejsciowejProkurentDlaKomorka : ITekstKwotyWejsciowejKonwerter
+    {
+        /// <summary>
+        /// Obsluguje zdarzenia wykonywane na zmiane konwertera.
+        /// </summary>
+        event EventHandler<ZmianaTekstKwotyWejsciowejKonwerter_ArgumentyZdarzenia> ZmianaTekstKwotyWejsciowejKonwerter_ObslugaZdarzenia;
+    }
+}
