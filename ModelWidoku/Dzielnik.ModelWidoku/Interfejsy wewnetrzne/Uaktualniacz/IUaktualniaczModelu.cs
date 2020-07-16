38472ef421e2d4f91485ba339c411c4a3900a236
Ungouge <zlobik89@gmail.com>
2020-07-16 21:48:25 +0200

    Dodano IModelUaktualniacz.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Interfejsy wewnetrzne/Uaktualniacz/IUaktualniaczModelu.cs
@@ -0,0 +1,16 @@
+﻿using Dzielnik.Zasoby.Interfejsy.KryteriumWyboru;
+using Dzielnik.Zasoby.Interfejsy.TablicaOsob;
+
+namespace Dzielnik.ModelWidoku.Interfejsy.Uaktualniacz
+{
+    /// <summary>
+    /// Interfejs dla uaktualniaczy modelu.
+    /// </summary>
+    internal interface IModelUaktualniacz
+    {
+        /// <summary>
+        /// Uaktulanij model.
+        /// </summary>
+        void Uaktualnij(ITablicaOsobBaza tablicaOsob, KryteriumWyboruTabeliNaleznosciWylicznik kryteriumWyboruTabeliNaleznosci);
+    }
+}
