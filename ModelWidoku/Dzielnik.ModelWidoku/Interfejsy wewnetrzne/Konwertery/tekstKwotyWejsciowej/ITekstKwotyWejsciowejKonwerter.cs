963c9fc4bcf88f40a8d02636585e01851578c383
Ungouge <zlobik89@gmail.com>
2020-07-16 23:12:59 +0200

    Dodano ITekstKwotyWejsciowejKonwerter.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Interfejsy wewnetrzne/Konwertery/tekstKwotyWejsciowej/ITekstKwotyWejsciowejKonwerter.cs
@@ -0,0 +1,16 @@
+﻿using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+namespace Dzielnik.ModelWidoku.Interfejsy.Konwertery
+{
+    /// <summary>
+    /// Interfejs dla konwerterow tekstu z komorki kwoty wejsciowe na odpowiedni typ nalznosci.
+    /// </summary>
+    internal interface ITekstKwotyWejsciowejKonwerter
+    {
+        /// <summary>
+        /// Konwertuje tekstu z komorki kwoty wejsciowe do odpowiedniego typu naleznosci w zalenosci od impementacji konwertera oraz przeslanego tekstu.
+        /// Zwrocoona wartosc wskazuje czy konwersja sie udala badz nie powiodla.
+        /// </summary>
+        bool Konwertuj(string tekstKwotyWejsciowej, out INaleznoscSwiadczenieBaza naleznosc);
+    }
+}
