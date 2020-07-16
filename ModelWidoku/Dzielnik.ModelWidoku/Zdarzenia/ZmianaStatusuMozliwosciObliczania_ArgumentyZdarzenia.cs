15a46f445f5a894b57de93e658eb98e014788345
Ungouge <zlobik89@gmail.com>
2020-07-16 21:43:14 +0200

    Dodano ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Zdarzenia/ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dzielnik.ModelWidoku.Zdarzenia
+{
+    /// <summary>
+    /// Argument zdarzenie dla obslugi zdarzenia notyfikacji zmiany moznosci obliczania przez model widoku.
+    /// </summary>
+    internal class ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia : EventArgs
+    {
+        public bool CzyMoznaObliczac { get; }
+
+        public static ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia WezPusty { get { return new ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia(true); } }
+
+        public new static readonly ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia Empty = WezPusty;
+
+        /// <summary>
+        /// Stworz argument notyfikacji zmiany moznosci obliczania przez model widoku.
+        /// </summary>
+        public ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia(bool czyMoznaObliczac)
+        {
+            CzyMoznaObliczac = czyMoznaObliczac;
+        }
+    }
+}
