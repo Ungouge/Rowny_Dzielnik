50a2f67869cd2f3e290439a4556e6564ad6d6ea9
Ungouge <zlobik89@gmail.com>
2020-07-16 23:17:39 +0200

    Dodano Czy_TekstKwotyWejsciowejNaleznosciPienieznejKonwerter_Konwertuj; oraz pomocnicze klasy do tworzenia potrzebnych testowalnych obiektow.

---
+++ Testy/ModelWidoku/Dzielnik.Testy.ModelWidoku.stworz/Konwertery/TekstKwotyWejsciowej/Stworz_TekstKwotyWejsciowejNaleznosciPienieznejKonwerter.cs
@@ -0,0 +1,25 @@
+﻿using Dzielnik.ModelWidoku.Interfejsy.Konwertery;
+using Dzielnik.ModelWidoku.Konwertery;
+using System;
+using Dzielnik.Testy.Zasoby.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+using Dzielnik.Zasoby.Interfejsy.Fabryki;
+using Dzielnik.Zasoby.Fabryki;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dzielnik.Testy.ModelWidoku.Konwertery.TekstKwotyWejsciowej
+{
+    internal static class Stworz_TekstKwotyWejsciowejNaleznosciPienieznejKonwerter
+    {
+        static readonly INaleznoscPieniezna naleznoscPienieznaZerowa = Stworz_NaleznoscPienieznaZerowa.Stworz();
+
+        static readonly IFabrykaNaleznoscPieniezna fabrykaNaleznoscPieniezna = new FabrykaNaleznoscPieniezna();
+
+        internal static  ITekstKwotyWejsciowejKonwerter Stworz()
+        {
+            return new TekstKwotyWejsciowejNaleznosciPienieznejKonwerter(fabrykaNaleznoscPieniezna, naleznoscPienieznaZerowa);
+        }
+    }
+}
---
+++ Testy/ModelWidoku/Dzielnik.Testy.ModelWidoku/Konwertery/TekstKwotyWejsciowej/Czy_TekstKwotyWejsciowejNaleznosciPienieznejKonwerter_Konwertuj.cs
@@ -0,0 +1,38 @@
+﻿using Dzielnik.ModelWidoku.Interfejsy.Konwertery;
+using Dzielnik.Zasoby.Interfejsy.Naleznosci;
+
+using Xunit;
+
+namespace Dzielnik.Testy.ModelWidoku.Konwertery.TekstKwotyWejsciowej
+{
+    public class Czy_TekstKwotyWejsciowejNaleznosciPienieznejKonwerter_Konwertuj
+    {
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("1", 100)]
+        [InlineData("2", 200)]
+        [InlineData("5", 500)]
+        [InlineData("0,00", 0)]
+        [InlineData("0,01", 1)]
+        [InlineData("0,02", 2)]
+        [InlineData("0,05", 5)]
+        [InlineData("1,00", 100)]
+        [InlineData("1,01", 101)]
+        [InlineData("1,02", 102)]
+        [InlineData("1,05", 105)]
+        public void Czy_Konwertuj_Zwraca_Prawidlowo(string tekstKwotyWejsciowej, int oczekiwaneSwiadzceniePieniezne)
+        {
+            //Ustal
+            ITekstKwotyWejsciowejKonwerter konwerter = Stworz_TekstKwotyWejsciowejNaleznosciPienieznejKonwerter.Stworz();
+
+            //Dzialaj
+            konwerter.Konwertuj(tekstKwotyWejsciowej, out INaleznoscSwiadczenieBaza naleznosc);
+
+            //Asercja
+            if (naleznosc is INaleznoscPieniezna naleznoscPieniezna)
+                Assert.Equal(oczekiwaneSwiadzceniePieniezne, naleznoscPieniezna.Swiadczenie);
+            else
+                Assert.IsAssignableFrom<INaleznoscPieniezna>(naleznosc);
+        }
+    }
+}
