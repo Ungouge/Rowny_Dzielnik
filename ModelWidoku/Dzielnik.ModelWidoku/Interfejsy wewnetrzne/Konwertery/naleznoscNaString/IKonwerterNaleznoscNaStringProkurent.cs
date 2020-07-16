3ad4fbc8ff20478dcf4f31d6fd93363a161c1979
Ungouge <zlobik89@gmail.com>
2020-07-16 23:43:16 +0200

    Dodano IKonwerterNaleznoscNaStringProkurent.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Interfejsy wewnetrzne/Konwertery/naleznoscNaString/IKonwerterNaleznoscNaStringProkurent.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Dzielnik.ModelWidoku.Interfejsy.Konwertery
+{
+    /// <summary>
+    /// Interfejs dla prokuretnta konwertera przeslanej naleznosci na tekstu w odpowiedni sposob dla przeslanej implementacji konwerteara.
+    /// </summary>
+    internal interface IKonwerterNaleznoscNaStringProkurent : INaleznoscNaStringKonwerter
+    {
+        /// <summary>
+        /// Uaktualnij implentacje konwertera ktora bedzie prokurowana.
+        /// </summary>
+        void UaktualnijProkurent(INaleznoscNaStringKonwerter konwerter);
+    }
+}
