3b30315556c1d2a17fd58f544ba4f9fbc923a149
Ungouge <zlobik89@gmail.com>
2020-07-16 21:36:58 +0200

    Dodano ZlyTypNaleznosciWyjatek.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Wyjatki/ZlyTypNaleznosciWyjatek.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using Dzielnik.Zasoby.Wyliczniki;
+
+namespace Dzielnik.Zasoby.Wyjatki
+{
+    /// <summary>
+    /// Wyjatek ten jest wyrzucany w przypadku, gdy zostanie przeslany lub zazadany typ naleznosci zawierajacy niewlasciwy typ swiadczenia. 
+    /// </summary>
+    public class ZlyTypNaleznosciWyjatek : Exception
+    {
+        public ZlyTypNaleznosciWyjatek()
+        {
+        }
+
+        public ZlyTypNaleznosciWyjatek(string message) :
+            base
+            (
+                message
+                )
+        {
+        }
+
+        public ZlyTypNaleznosciWyjatek(object klasaWKtorejNieZamplementowanoWyjatku, Type NieZamplementowanyInterfejs):
+            base
+            (
+                "Blednie przeslano " + NieZamplementowanyInterfejs.GetType().ToString() + " do " + klasaWKtorejNieZamplementowanoWyjatku.GetType().ToString() + "."
+            ) { }
+
+        public ZlyTypNaleznosciWyjatek(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ZlyTypNaleznosciWyjatek(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
