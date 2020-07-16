b07ca01bf914a1b9a0a83615d622a092fc6eb6ed
Ungouge <zlobik89@gmail.com>
2020-07-16 23:02:57 +0200

    Dodano Czy_ObliczKomenda_CanExecute_ZmianaWartosci; oraz pomocnicze klasy do tworzenia potrzebnych atrap.

---
+++ Testy/ModelWidoku/Dzielnik.Testy.ModelWidoku.stworzMocki/Komendy/Stworz_ObliczKomendaAkcja_WeryfikalnyMock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Moq;
+
+namespace Dzielnik.Testy.ModelWidoku.Komendy
+{
+    internal static class Stworz_ObliczKomendaAkcja_WeryfikalnyMock
+    {
+        internal static Mock<Action> Stworz_WeryfikalnyMock()
+        {
+            Mock<Action> naObliczKomendaAkcja = new Mock<Action>();
+
+            naObliczKomendaAkcja.
+                Setup
+                (
+                    akcja => akcja.Invoke()
+                ).Verifiable();
+
+            return naObliczKomendaAkcja;
+        }
+    }
+}
---
+++ Testy/ModelWidoku/Dzielnik.Testy.ModelWidoku/Komendy/Czy_ObliczKomenda_CanExecute_ZmianaWartosci.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Dzielnik.ModelWidoku.Interfejsy.CzyMoznaObliczac;
+using Dzielnik.ModelWidoku.Interfejsy.Komendy;
+using Dzielnik.ModelWidoku.Interfejsy.Komorki;
+using Dzielnik.Testy.ModelWidoku.CzyMoznaObliczac;
+
+using Moq;
+using Xunit;
+
+namespace Dzielnik.Testy.ModelWidoku.Komendy
+{
+    public static class Czy_ObliczKomenda_CanExecute_ZmianaWartosci
+    {
+        [Fact]
+        public static void Czy_CanExecute_ZmianaWartosci_ZmieniaWartoscNa_Prawde()
+        {
+            //Ustal, Dzialaj i Asercja
+            Czy_CanExecute_ZmianaWartosci_ZmieniaWartoscNa(true);
+        }
+
+        [Fact]
+        public static void Czy_CanExecute_ZmianaWartosci_ZmieniaWartoscNa_Falsz()
+        {
+            //Ustal, Dzialaj i Asercja
+            Czy_CanExecute_ZmianaWartosci_ZmieniaWartoscNa(false);
+        }
+
+        private static void Czy_CanExecute_ZmianaWartosci_ZmieniaWartoscNa(bool czyPozwalam)
+        {
+            //Ustal
+            Mock<Action> naObliczKomendaAkcja = Stworz_ObliczKomendaAkcja_WeryfikalnyMock.Stworz_WeryfikalnyMock();
+
+            ICzyMoznaObliczacWkaznik czyMoznaObliczacWkaznik = Stworz_CzyMoznaObliczacWkaznik.Stworz();
+
+            IObliczKomenda komenda = Stworz_ObliczKomenda.Stworz(czyMoznaObliczacWkaznik);
+
+            //Dzialaj
+            czyMoznaObliczacWkaznik.ZmienMozliwoscOblicznia(default(IKomorkaWejsciowaKwoty), czyPozwalam);
+
+            //Asercja
+            Assert.Equal(czyPozwalam, komenda.CanExecute(default(object)));
+        }
+    }
+}
