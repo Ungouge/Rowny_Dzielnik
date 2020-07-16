4d406402b3f340bada1ca68f7dbb2d82e363e46d
Ungouge <zlobik89@gmail.com>
2020-07-16 21:33:02 +0200

    Dodano ZmianaUstawieniaIloscOsob_ArgumentyZdarzenia.

---
+++ Zasoby/Dzielnik.Zasoby.Interfejsy/Zdarzenia/ustawienia/ZmianaUstawieniaIloscOsob_ArgumentyZdarzenia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dzielnik.Zasoby.Zdarzenia
+{
+    /// <summary>
+    /// Argument zadrzenia dla obslugi zmiany ilosci osob w ustawieniach.
+    /// </summary>
+    public class ZmianaUstawieniaIloscOsob_ArgumentyZdarzenia : EventArgs
+    {
+        public byte IloscOsob { get; }
+
+        private static ZmianaUstawieniaIloscOsob_ArgumentyZdarzenia WezPusty => new ZmianaUstawieniaIloscOsob_ArgumentyZdarzenia(4);
+
+        public static new readonly ZmianaUstawieniaIloscOsob_ArgumentyZdarzenia Empty = WezPusty;
+
+        /// <summary>
+        /// Stworz argument zmiany ilosci osob w ustawieniach.
+        /// </summary>
+        public ZmianaUstawieniaIloscOsob_ArgumentyZdarzenia(byte iloscOsob)
+        {
+            IloscOsob = iloscOsob;
+        }
+    }
+}
