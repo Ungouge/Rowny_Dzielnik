3a1e4010ef9b5c1e818b442aeef798ea4dcc32c4
Ungouge <zlobik89@gmail.com>
2020-07-16 22:59:28 +0200

    Dodano ObliczKomenda.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Komendy/ObliczKomenda.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Dzielnik.ModelWidoku.Interfejsy.CzyMoznaObliczac;
+using Dzielnik.ModelWidoku.Interfejsy.Komendy;
+using Dzielnik.ModelWidoku.Zdarzenia;
+
+namespace Dzielnik.ModelWidoku.Komendy
+{
+    /// <summary>
+    /// Interfejs dla przcisku obliczania komendy.
+    /// </summary>
+    internal class ObliczKomenda : IObliczKomenda
+    {
+        private readonly Action naObliczKomendaAkcja;
+
+        bool czyMoznaObliczac;
+
+        public ObliczKomenda(Action naObliczKomendaAkcja, ICzyMoznaObliczacZmianaWartosci czyMoznaObliczacWkaznik)
+        {
+            this.naObliczKomendaAkcja = naObliczKomendaAkcja;
+
+            czyMoznaObliczacWkaznik.ZmianaWartosci += CzyMoznaObliczacWkaznik_ZmianaWartosci;
+
+            czyMoznaObliczac =  true;
+        }
+
+        private void CzyMoznaObliczacWkaznik_ZmianaWartosci(object sender, ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia e)
+        {
+            if (czyMoznaObliczac == e.CzyMoznaObliczac)
+                return;
+
+            czyMoznaObliczac = e.CzyMoznaObliczac;
+
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler CanExecuteChanged;
+        
+        public bool CanExecute(object parameter)
+        {
+            return czyMoznaObliczac;
+        }
+
+        public void Execute(object parameter)
+        {
+            naObliczKomendaAkcja();
+        }
+    }
+}
