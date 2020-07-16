d3e0b29b0bdf28b2a61a3311a684697282f57cf7
Ungouge <zlobik89@gmail.com>
2020-07-16 20:43:49 +0200

    Dodano CzyMoznaObliczacWkaznik.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/CzyMoznaObliczac/CzyMoznaObliczacWkaznik.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Dzielnik.ModelWidoku.Interfejsy.CzyMoznaObliczac;
+using Dzielnik.ModelWidoku.Interfejsy.Komorki;
+using Dzielnik.ModelWidoku.Zdarzenia;
+
+namespace Dzielnik.ModelWidoku.CzyMoznaObliczac
+{
+    /// <summary>
+    /// Wskaznik wskazujacy czy mozna obliczac.
+    /// </summary>
+    internal partial class CzyMoznaObliczacWkaznik : ICzyMoznaObliczacWkaznik
+    {
+        private bool czyMoznaObliczacWkaznik = true;
+
+        private readonly List<IKomorkaWejsciowaKwoty> zabraniajacaKomorkiKwotyWejsciowej = new List<IKomorkaWejsciowaKwoty>();
+
+        public event EventHandler<ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia> ZmianaWartosci;
+
+        public bool CzyMoznaObliczac
+        {
+            get
+            {
+                return czyMoznaObliczacWkaznik;
+            }
+            private set
+            {
+                if (czyMoznaObliczacWkaznik == value)
+                    return;
+
+                czyMoznaObliczacWkaznik = value;
+
+                NaZmianeWlasciwosci();
+            }
+        }
+
+        protected void NaZmianeWlasciwosci()
+        {
+            ZmianaWartosci?.Invoke(this, new ZmianaStatusuMozliwosciObliczania_ArgumentyZdarzenia(CzyMoznaObliczac));
+        }
+    }
+}
---
+++ ModelWidoku/Dzielnik.ModelWidoku/CzyMoznaObliczac/ZmienMozliwoscOblicznia.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+using Dzielnik.ModelWidoku.Interfejsy.Komorki;
+
+namespace Dzielnik.ModelWidoku.CzyMoznaObliczac
+{
+    partial class CzyMoznaObliczacWkaznik
+    {
+        public void ZmienMozliwoscOblicznia(IKomorkaWejsciowaKwoty komorkaKwotyWejsciowej, bool czyPozwala)
+        {
+            if (czyPozwala == true)
+                ZezwolMozliwoscOblicznia(komorkaKwotyWejsciowej);
+            else
+                ZabronMozliwoscOblicznia(komorkaKwotyWejsciowej);
+        }
+
+        private void ZezwolMozliwoscOblicznia(IKomorkaWejsciowaKwoty komorkaKwotyWejsciowej)
+        {
+            zabraniajacaKomorkiKwotyWejsciowej.RemoveAll(komorka => komorka == komorkaKwotyWejsciowej);
+
+            if (zabraniajacaKomorkiKwotyWejsciowej.Any() == false)
+                CzyMoznaObliczac = true;
+        }
+
+        private void ZabronMozliwoscOblicznia(IKomorkaWejsciowaKwoty komorkaKwotyWejsciowej)
+        {
+            zabraniajacaKomorkiKwotyWejsciowej.Add(komorkaKwotyWejsciowej);
+
+            CzyMoznaObliczac = false;
+        }
+    }
+}
