d76a7364dca3b9bbebf1dd1ee5b95206d16bb4bb
Ungouge <zlobik89@gmail.com>
2020-07-16 23:14:59 +0200

    Dodano ZmianaTekstKwotyWejsciowejKonwerter_ArgumentyZdarzenia.

---
+++ ModelWidoku/Dzielnik.ModelWidoku/Zdarzenia/ZmianaTekstKwotyWejsciowejKonwerter_ArgumentyZdarzenia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dzielnik.ModelWidoku.Zdarzenia
+{
+    /// <summary>
+    /// Argument zdarzenia zmiany konwertera tekstu kwoty wejsciowej.
+    /// </summary>
+    public class ZmianaTekstKwotyWejsciowejKonwerter_ArgumentyZdarzenia : EventArgs
+    {
+        private static ZmianaTekstKwotyWejsciowejKonwerter_ArgumentyZdarzenia WezPusty =>
+            new ZmianaTekstKwotyWejsciowejKonwerter_ArgumentyZdarzenia();
+
+        public static new readonly ZmianaTekstKwotyWejsciowejKonwerter_ArgumentyZdarzenia Empty = WezPusty;
+    }
+}
