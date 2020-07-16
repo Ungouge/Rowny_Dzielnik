355a03824141a8edee541b6238ae89d88e166cf3
Ungouge <zlobik89@gmail.com>
2020-07-16 23:03:30 +0200

    Dodano Czy_ObliczKomenda_Execute.

---
+++ Testy/ModelWidoku/Dzielnik.Testy.ModelWidoku/Komendy/Czy_ObliczKomenda_Execute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Dzielnik.ModelWidoku.Interfejsy.Komendy;
+
+using Moq;
+using Xunit;
+
+namespace Dzielnik.Testy.ModelWidoku.Komendy
+{
+    public class Czy_ObliczKomenda_Execute
+    {
+        [Fact]
+        public void Czy_Execute_Wykonuje()
+        {
+            //Ustal
+            Mock<Action> naObliczKomendaAkcja = Stworz_ObliczKomendaAkcja_WeryfikalnyMock.Stworz_WeryfikalnyMock();
+
+            IObliczKomenda komenda = Stworz_ObliczKomenda.Stworz(naObliczKomendaAkcja.Object);
+
+            //Dzialaj
+            komenda.Execute(default(object));
+
+            //Asercja
+            naObliczKomendaAkcja.Verify( x => x.Invoke(), Times.Once, "Komenda \"" + naObliczKomendaAkcja.GetType().ToString() + "\" nie wykonala przekazanej akcji"  );
+        }
+    }
+}
